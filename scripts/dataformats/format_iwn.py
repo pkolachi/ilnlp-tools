072d1cf7eea3b0cec1b1e04c39eede3caa30f385
Prasanth Kolachina <kolachina.prasanth@gmail.com>
2019-08-22 19:50:33 +0200

    add skeleton file to format IWN

---
+++ scripts/dataformats/format_iwn.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Reads multilingual dictionaries from IWN and re-formats them into 
+multiple tsv files.
+"""
+
+def main() :
+  return 
+
+if __name__ == '__main__' :
+  main() 
